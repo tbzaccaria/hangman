343b9fcc77d1a4b5c3c426f746cc6cd409363872
Tabi Zaccaria <zaccariatabi@gmail.com>
2021-09-29 10:30:10 +0200

    first commit

---
+++ hangman.md
@@ -0,0 +1,50 @@
+# Hangman
+
+- Type of challenge: **consolidation**
+- Repository: `Hangman`
+- Challenge: **solo**
+- Duration : **3 days**
+- Deadline : **29/09/2021**
+
+## The mission
+
+You have to make a website version of the [hangman](https://en.wikipedia.org/wiki/Hangman_(game)) word game. To do so
+try to think the *structure*, the "*backend*" (functionality) and the *frontend*
+(design) of your project before you start coding.
+
+**At the very least your version of the *hangman* should:**
+
+- propose an hidden word
+- allow the selection of letters
+- verify if a selected letter belongs to the word
+- count the number of failure (lives)
+- check if it's a *game over*
+- check if the word as been found
+
+**Optionally it can**:
+
+- keep in memory all guessed letters
+- only let the player guess a letter once
+- add an image for each step
+- check keyboard recognition
+
+**Bonus**:
+
+- use canvas
+
+NOTE: The instructions below are meant to guide you not impose a way of doing
+things, what matters is the end result.
+
+**Instructions**:
+
+* create the repository
+* think the project through
+* make it functional
+* write the frontend (make it pretty)
+* implement both parts into a web page
+
+## Resources
+
+* [Example](https://hangmanwordgame.com/?fca=1&success=0#/)
+
+
---
+++ script.js
@@ -0,0 +1,141 @@
+//challenge HANGMAN script
+
+(() => {
+
+    function getRandomArbitrary(min,max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max-min)) + min;
+}
+
+    function chooseWord(){
+        let array=["bateau","voiture","fromage","ceinture","chien","histoire","anatomie","becode","ordinateur","maison","orientation","navire","ville","village","avion","javascript","septembre","novembre","pluie","soleil","physique"];
+        let max=(array.length)-1;
+        let index= getRandomArbitrary(0,max);
+        let word=array[index];
+
+        return word;
+
+    }
+    // init mot caché + init variable
+    let turn=0;
+    let keyword =chooseWord();
+    
+    console.log(keyword);
+    let data=document.getElementById("keyword");
+    data.setAttribute("key-data",keyword);
+    let key = document.getElementsByTagName("span")[1].getAttribute("key-data");
+    
+    console.log(key);
+    let answer =document.getElementById("name").value;
+    let isThere = key.split("");
+    let hide="";
+    for (let elem of isThere){
+        hide=hide+"-";
+    }
+    document.getElementById("keyword").innerHTML = hide;
+    console.log(isThere);
+
+    let shots=Number(document.getElementById("shots").innerText);
+    let memoire=[];
+
+    // button : add the previous (if there is) to the new reveled letters
+    document.getElementById("run").addEventListener("click",function(){
+
+        
+        let key = document.getElementsByTagName("span")[1].getAttribute("key-data");
+        console.log(key);
+        let answer =document.getElementById("name").value;
+        let isThere = key.split("");
+        console.log(isThere);
+        let guess=false;
+        let good="";
+        hide="";
+        
+        
+        
+        // verify if a selected letter belongs to the word
+        for(let i=0;i<isThere.length;i=i+1){
+            if (isThere[i]===answer){
+                document.getElementById("name").style.borderColor ="green";
+                console.log("green");
+                console.log(isThere[i]);
+                guess=true;
+                good=isThere[i];
+                let hideSplit=hide.split();
+                hide=hide+good;
+               
+               
+            }
+            else{
+                hide=hide+"-";
+                
+            }
+            
+        }
+        if (guess===false){
+            document.getElementById("name").style.borderColor ="red";
+        }
+        memoire.push(hide);
+        document.getElementById("keyword").innerHTML=hide;
+        // count the number of failures 
+        if (guess===false){
+            shots=shots-1;
+            shots=shots.toString();
+            document.getElementById("shots").innerHTML = shots;
+        }
+
+        console.log(memoire);
+        let previousHide="";
+        
+        // add the memory of the previous letters to the new one
+        console.log(memoire);
+        console.log(hide);
+        if (turn>0){
+            for(i=0;i<isThere.length;i=i+1){
+                if((memoire[turn-1][i]!="-") && (memoire[turn][i]==="-")){
+                    previousHide=previousHide+memoire[turn-1][i];
+                }
+                else if((memoire[turn-1][i]==="-") && (memoire[turn][i]!="-")){
+                    previousHide=previousHide+memoire[turn][i];
+                }
+                else if(memoire[turn-1][i]===memoire[turn][i]){
+                    previousHide=previousHide+memoire[turn-1][i];
+                }
+                else{
+                    previousHide=previousHide+"-";
+                    
+                }
+
+            }
+        }
+        console.log(previousHide);
+        if (turn>0){
+
+            memoire.pop();
+            memoire.push(previousHide);
+            document.getElementById("keyword").innerHTML=previousHide;
+        }
+        turn=turn+1;
+        // check if it's a game over
+        if (shots<1){
+            alert("You lose. Retry !");
+            location.reload();
+        }
+        console.log(keyword);
+        console.log(previousHide);
+        console.log(key);
+        // check if the word as been found
+        if (previousHide===key){
+            alert("You win! Now a new word !");
+            location.reload();
+        }
+    });
+
+
+
+
+
+
+
+})();
